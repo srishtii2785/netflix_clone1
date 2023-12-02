7494cfc60e727807696699d07801674fb6ebb92e
srishtii2785 <152674306+srishtii2785@users.noreply.github.com>
2023-12-02 10:23:05 +0530

    Add files via upload

---
+++ netflixlogo.jsx
@@ -0,0 +1,16 @@
+>component>netflix>NetflixLogo.jsx>NetflixLogo
+import React from "react";
+import './style.css';
+export const NetflixLogo= ()=> {
+	return(
+		<div class="logo-container">
+			<span class="left"></span>
+			<span class="center"></span>
+		    <span class="center"></span>
+		</div>);
+};
+
+
+
+
+
